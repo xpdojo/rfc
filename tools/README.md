190fb5576750a3e18afcee1b95852eaa9032c69d
Changsu Im <imcxsu@gmail.com>
2023-02-04 18:58:12 +0900

    add output format argument

--- tools/README.md
+++ tools/README.md
@@ -11,6 +11,6 @@ python3 -m pip install -r requirements.txt
 ## Usage
 
 ```sh
-# python3 main.py -sn ${RFC_SERIAL_NUMBER} -o ${OUTPUT_DIRECTORY}
- python3 main.py -sn 1034 -o ..
+# python3 main.py -sn ${RFC_SERIAL_NUMBER} -o ${OUTPUT_DIRECTORY} -t ${OUTPUT_FORMAT}
+python3 main.py -sn 1034 -o .. -t txt
 ```
--- tools/main.py
+++ tools/main.py
@@ -6,45 +6,51 @@ from rfc2html import markup
 
 
 def main(
-        output_directory: str,
         serial_number: int,
+        output_directory: str,
+        to: str,
 ) -> None:
-    """
-    Download RFC txt and convert it to HTML and Markdown.
 
-    :param output_directory: Directory to save the RFC files.
-    :param serial_number: RFC number
-    """
     rfc_name = f'rfc{serial_number}'
-    text = requests.get(f"https://www.rfc-editor.org/rfc/{rfc_name}.txt").text
-    text1 = re.sub(r'(?<=[a-z., ]{2})\n(?!\n)', '', text)  # Remove single line breaks
+    text = requests.get(f"https://www.ietf.org/rfc/{rfc_name}.txt").text
+    reformat = re.sub(r'(?<=[a-z., ]{2})\n(?!\n)', '', text)  # Remove single line breaks
 
-    with open(f'{output_directory}/{rfc_name}.txt', 'w') as f:
-        f.write(text1)
+    if to == "txt":
+        with open(f'{output_directory}/{rfc_name}.txt', 'w') as f:
+            f.write(reformat)
+        return
 
-    html = markup(text1)
-    with open(f'{output_directory}/{rfc_name}.html', 'w') as f:
-        f.write(html)
+    html = markup(reformat)
+    if to == "html":
+        with open(f'{output_directory}/{rfc_name}.html', 'w') as f:
+            f.write(html)
+        return
 
-    with open(f'{output_directory}/{rfc_name}.md', 'w') as f:
-        f.write(md(html))
+    print('test')
+    if to == "markdown":
+        with open(f'{output_directory}/{rfc_name}.md', 'w') as f:
+            f.write(md(html))
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="""
-    Download RFC txt and convert it to HTML and Markdown.
+    Download RFC txt and convert it to other format.
     """)
-    parser.add_argument('-o', '--output-directory', metavar='<str>', required=False,
-                        default='.', type=str,
-                        help='Output directory')
     parser.add_argument('-sn', '--serial-number', metavar='<int>', required=True,
                         default=None, type=int,
                         help='RFC serial number')
+    parser.add_argument('-o', '--output-directory', metavar='<str>', required=False,
+                        default='.', type=str,
+                        help='Output directory')
+    parser.add_argument('-t', '--to', metavar='<str>', required=False,
+                        default='txt', type=str,
+                        help='Output format')
 
     args = parser.parse_args()
     main(
-        output_directory=args.output_directory,
         serial_number=args.serial_number,
+        output_directory=args.output_directory,
+        to=args.to,
     )
