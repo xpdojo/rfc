4e45be698a250594d7e26d9e6064aaf2e0b4729a
Changsu Im <imcxsu@gmail.com>
2023-02-04 14:37:42 +0900

    convert to markdown

--- tools/README.md
+++ tools/README.md
@@ -11,5 +11,6 @@ python3 -m pip install -r requirements.txt
 ## Usage
 
 ```sh
-python3 main.py -sn ${RFC_SERIAL_NUMBER}
+# python3 main.py -sn ${RFC_SERIAL_NUMBER} -o ${OUTPUT_DIRECTORY}
+ python3 main.py -sn 1034 -o ..
 ```
--- tools/main.py
+++ tools/main.py
@@ -1,36 +1,50 @@
+import re
+
 import requests
+from markdownify import markdownify as md
 from rfc2html import markup
 
 
-def main(serial_number: int) -> None:
+def main(
+        output_directory: str,
+        serial_number: int,
+) -> None:
     """
-    Download RFC txt and convert it to HTML.
+    Download RFC txt and convert it to HTML and Markdown.
 
+    :param output_directory: Directory to save the RFC files.
     :param serial_number: RFC number
-    :return:
     """
     rfc_name = f'rfc{serial_number}'
     text = requests.get(f"https://www.rfc-editor.org/rfc/{rfc_name}.txt").text
+    text1 = re.sub(r'(?<=[a-z., ]{2})\n(?!\n)', '', text)  # Remove single line breaks
 
-    txt_file_name = f'{rfc_name}.txt'
-    with open(txt_file_name, 'w') as f:
-        f.write(text)
+    with open(f'{output_directory}/{rfc_name}.txt', 'w') as f:
+        f.write(text1)
 
-    html = markup(text)
-    html_file_name = f'{rfc_name}.html'
-    with open(html_file_name, 'w') as f:
+    html = markup(text1)
+    with open(f'{output_directory}/{rfc_name}.html', 'w') as f:
         f.write(html)
+
+    with open(f'{output_directory}/{rfc_name}.md', 'w') as f:
+        f.write(md(html))
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="""
-    Bulk Indexing Oracle data to Elasticsearch
+    Download RFC txt and convert it to HTML and Markdown.
     """)
+    parser.add_argument('-o', '--output-directory', metavar='<str>', required=False,
+                        default='.', type=str,
+                        help='Output directory')
     parser.add_argument('-sn', '--serial-number', metavar='<int>', required=True,
                         default=None, type=int,
                         help='RFC serial number')
 
     args = parser.parse_args()
-    main(serial_number=args.serial_number)
+    main(
+        output_directory=args.output_directory,
+        serial_number=args.serial_number,
+    )
